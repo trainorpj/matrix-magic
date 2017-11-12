5a746538807880c6339989fae62999465130e0b8
PJ Trainor <trainorpj@gmail.com>
2017-11-11 21:10:40 -0500

    add rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -3,14 +3,29 @@ import babel from "rollup-plugin-babel";
 
 export default {
   input: "src/index.js",
-  output: {
-    file: "build/bundle.js",
-    format: "cjs"
-  },
+  output: [
+    {
+      file: "build/index.cjs.js",
+      format: "cjs"
+    },
+    {
+      file: "build/index.es.js",
+      format: "es"
+    }
+  ],
   plugins: [
     resolve(),
     babel({
-      exclude: "node_modules/**" // only transpile our source code
+      presets: [
+        [
+          "env",
+          {
+            modules: false
+          }
+        ]
+      ],
+      plugins: ["external-helpers"],
+      babelrc: false
     })
   ]
 };
